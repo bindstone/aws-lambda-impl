0755b9defaddb7c892ee8899dad6654929fb2687
bindstone <msbindstone@gmail.com>
2023-03-16 23:28:38 +0000

    Init Project

--- dart-impl/bin/main.dart
+++ dart-impl/bin/main.dart
@@ -1,20 +1,23 @@
-import 'package:aws_lambda_dart_runtime/aws_lambda_dart_runtime.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
-void main() async {
+Future<void> main() async {
 
-  upperCase(context, event) async {
-    var requestBody = event.body;
-    String name = requestBody["name"];
+  final port = 8080;
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, port);
+  print('Listening on port ${server.port}');
 
-    final response = AwsApiGatewayResponse(
-      body: name.toUpperCase(),
-      statusCode: 200,
-    );
-
-    return response;
+  await for (HttpRequest request in server) {
+    // print(request.uri.path);
+    // print(request.method);
+    Future<String> content = utf8.decodeStream(request);
+    content.then((value) {
+      var data = jsonDecode(value);
+      request.response
+        ..headers.contentType = ContentType.text
+        ..write(data['name'].toUpperCase())
+        ..close();
+    });
   }
-
-  Runtime()
-    ..registerHandler<AwsApiGatewayEvent>("uppercase", upperCase)
-    ..invoke();
 }
